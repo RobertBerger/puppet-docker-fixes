3330dc9e686158a105fb767aa7478b013f9c4bc0
Robert Berger <robert.berger@ReliableEmbeddedSystems.com>
2014-05-07 16:43:45 +0300

    some fix sudo permissions

--- README.markdown
+++ README.markdown
@@ -1,3 +1,5 @@
 # docker_fixes #
 
-This is the docker_fixes module. It provides...
+This is the docker_fixes module. 
+
+It's an attempt to fix a few things which are screwed in docker images.
--- manifests/init.pp
+++ manifests/init.pp
@@ -5,11 +5,26 @@
 # Parameters: none
 #
 # Actions:
+#         * fix permissions for /usr/bin/sudo
 #
 # Requires: see Modulefile
 #
 # Sample Usage:
 #
-class docker_fixes {
 
+class docker_fixes (
+  $package_ensure = 'present'
+){
+    
+    # the permission of /usr/bin/sudo are screwed up
+    # let's fix them!
+
+    package { 'sudo':
+        ensure => $package_ensure,
+    }
+
+    file { '/usr/bin/sudo':
+      ensure => present,
+      mode   => '4755',
+    }
 }
